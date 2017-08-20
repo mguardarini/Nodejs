15b3310c4db66ff7e43f7868e0d545286637bdb7
mauro guardarini <mauro.guardarini@gmail.com>
2017-08-20 17:05:34 -0300

    Code Performance

--- Bitcoin API/server.js
+++ Bitcoin API/server.js
@@ -4,18 +4,14 @@ const argv = require('yargs').argv
 var request = require('request');
 
 function getQuote() {
-
     // var url = 'https://api.coindesk.com/v1/bpi/currentprice/CNY.json';
     var url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-        //this variable i use to test the second 'if' (getStatusCode)    
+        //this variable i use to test the second 'if' (getStatusCode)
         // var url = "https://foo"
-
     return new Promise(function(resolve, reject) {
-        //request for api 
+        //request for api
         request(url,
-
             function(error, response, body) {
-
                 if (error) {
                     return reject(error);
                 } else {
@@ -28,7 +24,7 @@ function getQuote() {
                             resolve(body);
                         }
                     } catch (error) {
-                        console.log("Unknown Error: " + error);
+                        console.log("Unknown Error[API]: " + error);
                     }
                 }
             });
@@ -38,38 +34,26 @@ function getQuote() {
 async function main() {
 
     var quote = await getQuote();
-
     var data = JSON.parse(quote);
+    var eur = data.bpi.EUR.rate;
+    var usd = data.bpi.USD.rate;
+    var gbp = data.bpi.GBP.rate;
+    var disclaimer = data.disclaimer;
 
-    console.log("\n******CoinDesk Bitcoin Price Index API******" +
-        "\nDisclaimer: " + data.disclaimer + "\n"
+
+    console.log("\n******CoinDesk Bitcoin Price Index API******\n" +
+        "\nDisclaimer: " + disclaimer + "\n"
     );
 
-    switch (argv.BPI) {
-
-        case argv.BPI = 'USD':
-            {
-                console.log("\nThe Bitcoin price in United States Dollar: " + data.bpi.USD.rate + "\n");
-                break;
-            }
-        case argv.BPI = 'EUR':
-            {
-                console.log("\nThe Bitcoin price in Euro: " + data.bpi.EUR.rate + "\n");
-                break;
-            }
-        case argv.BPI = 'GBP':
-            {
-                console.log("\nThe Bitcoin price in British Pound Sterling: " + data.bpi.GBP.rate + "\n");
-                break;
-            }
-        default:
-            {
-                console.log("Sorry, I don't identify this option, please utilize this options USD, EUR or GBP follow the example: node server.js --BPI=USD\n");
-                break;
-            }
+    if(argv.BPI == 'USD'){
+        console.log("\nThe Bitcoin price in United States Dollar: " + usd + "\n");
+    }else if (argv.BPI == 'EUR') {
+        console.log("\nThe Bitcoin price in Euro: " + eur + "\n");
+    }else if (argv.BPI == 'GBP') {
+        console.log("\nThe Bitcoin price in British Pound Sterling: " + gbp + "\n");
+    }else{
+      console.log("Sorry, I don't identify this option, please utilize this options USD, EUR or GBP follow the example: node server.js --BPI=USD\n");
     }
-}
 
+}
 main();
-//I export this module because i test with mocha framework.
-exports.module = getQuote;
